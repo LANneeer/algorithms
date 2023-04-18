8cc03ebfc79aa473b3ce3a14e750816c1b512c46
LANneeer <0penguin@mail.ru>
2023-04-18 23:36:30 +0600

    add queue structure

---
+++ structures/queue.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+class Queue:
+    """
+    Queue is a data structure which uses array as a line of queue,
+    queue uses FIFO concept (First In First Out)
+    """
+    def __init__(self):
+        self.items = deque()
+
+    def enqueue(self, item) -> None:
+        self.items.append(item)
+
+    def enqueue(self) -> object:
+        return self.items.popleft()
